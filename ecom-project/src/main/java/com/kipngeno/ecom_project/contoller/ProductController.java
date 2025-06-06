1f892aa8758f4d249e9844aa457b453cfa6d4b49
brian070069 <kkipngeno409@gmail.com>
2025-06-07 00:29:58 +0300

    using ResponseEntityAnnotation and getting products changes

--- ecom-project/src/main/java/com/kipngeno/ecom_project/contoller/ProductController.java
+++ ecom-project/src/main/java/com/kipngeno/ecom_project/contoller/ProductController.java
@@ -3,10 +3,9 @@ package com.kipngeno.ecom_project.contoller;
 import com.kipngeno.ecom_project.model.Product;
 import com.kipngeno.ecom_project.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,14 +17,19 @@ public class ProductController {
     @Autowired
     private ProductService service;
 
-    @RequestMapping("/")
-    public String greet(){
-        return "Hello World!";
+    @GetMapping("/products")
+    public ResponseEntity<List<Product>> getAllProducts() {
+        return new ResponseEntity<>(service.getAllProducts(), HttpStatus.OK);
     }
 
-    @GetMapping("/products")
-    public List<Product> getAllProducts() {
-        return service.getAllProducts();
+    @GetMapping("/product/{id}")
+    public ResponseEntity<Product> getProduct(@PathVariable int id) {
+        Product product = service.getProductById(id);
+        if(product != null)
+            return new ResponseEntity<>(product,HttpStatus.OK);
+        else
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+
     }
 
 }
